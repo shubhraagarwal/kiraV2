26d82996e954dee88212a0f8f95369c7ace7c56e
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-04-15 02:18:17 +0530

    increased min-height

--- src/pages/Assent.jsx
+++ src/pages/Assent.jsx
@@ -1,16 +1,31 @@
 import SuperDetails from "../components/SuperDetails";
 import Navbar from "../components/Navbar";
-import AssentModel from '../assets/03.glb'
+import AssentModel from "../assets/03.glb";
 import Staking from "../assets/staking.png";
 const Assent = () => {
-    return (  
-        <>
-         <div className="navBox">
-         <Navbar home = '' market ='' purpose = {Staking} development = '' reserve = '' staking = '' weapon = '' land = ''/>
-        </div>
-        <SuperDetails class="Assent" title="ASSENT" mod = 'Model' img={AssentModel} para = "Cultivate KIRA community. We are calling all defenders of the forgotten to embark in this battle with us. Thoroughout this journey we will form plans and partnerships, gather resources, create visions the masses can explore through art and visual expression and most importantly obtain our Freedom."/>
-        </>
-    );
-}
- 
+  return (
+    <>
+      <div className="navBox">
+        <Navbar
+          home=""
+          market=""
+          purpose={Staking}
+          development=""
+          reserve=""
+          staking=""
+          weapon=""
+          land=""
+        />
+      </div>
+      <SuperDetails
+        class="Assent"
+        title="ASSENT"
+        mod="Model"
+        img={AssentModel}
+        para="Cultivate KIRA community. We are calling all defenders of the forgotten to embark in this battle with us. Thoroughout this journey we will form plans and partnerships, gather resources, create visions the masses can explore through art and visual expression and most importantly obtain our Freedom."
+      />
+    </>
+  );
+};
+
 export default Assent;
--- src/pages/Staking.jsx
+++ src/pages/Staking.jsx
@@ -1,21 +1,20 @@
 import Navbar from "../components/Navbar";
-import Inventory from '../components/Inventory'
+import Inventory from "../components/Inventory";
 import StakeFoot from "../components/StakeFoot";
-import Staking from "../assets/staking.png";
-const Staking = () => {
-    return (  
-        <>
-        <div className="navBox">
-     <Navbar/>
+// import Staking from "../assets/staking.png";
+
+export const Staking = () => {
+  return (
+    <>
+      <div className="navBox">
+        <Navbar />
       </div>
       <div>
-      <div>
-      <Inventory/>
+        <div>
+          <Inventory />
+        </div>
       </div>
-      </div>
-      <StakeFoot/>
-        </>
-    );
-}
- 
-export default Staking;
+      <StakeFoot />
+    </>
+  );
+};
