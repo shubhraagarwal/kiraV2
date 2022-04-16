91b7692db4fc5a05e3428df8474d37e00c182f47
shubhraagarwal <agarwalshubhra2002@gmail.com>
2022-04-16 10:58:20 +0530

    reserve text added, marketplace image changed

--- src/pages/Chart.jsx
+++ src/pages/Chart.jsx
@@ -1,35 +1,46 @@
 import Navbar from "../components/Navbar";
-import Bars from '../assets/statsBlur.png'
-import ChartBlur from '../assets/chartBlur.png'
-import TBA from '../assets/TBA.png'
+import Bars from "../assets/statsBlur.png";
+import ChartBlur from "../assets/chartBlur.png";
+import TBA from "../assets/TBA.png";
 import Staking from "../assets/staking.png";
 const Chart = () => {
   return (
     <>
       <div className="navBox">
-      <Navbar home = '' market ='' purpose = '' development = '' reserve = {Staking} staking = '' weapon = '' land = ''/>
+        <Navbar
+          home=""
+          market=""
+          purpose=""
+          development=""
+          reserve={Staking}
+          staking=""
+          weapon=""
+          land=""
+        />
       </div>
 
       <div className="chart">
-      <div>
-        <p>
-          KIRA is an ongoing movement and to defeat the Shogun we will need to
-          deploy funds to develop and grow. KIRA's earn together and work
-          together to raise awareness and build a reserve of currency that goes
-          towards the project and end goal. The Treasury (royalties) will be
-          used primarily to build and deploy incentives and advancements to the
-          community, starting off with weekly deposits to the KIRA RESERVE. KIRA
-          RESERVE is built exclusively for holders and will not be used by the
-          team for anything that does not pertain to bettering KIRA as a
-          project.
-        </p>
-        <h1>Tokenomics</h1>
-        <img src={ChartBlur} alt=""  className="Chartblur"/>
-        <img src={TBA} alt="" id="aboveChart"/>
+        <div>
+          <p>
+            <strong>KIRA</strong> is an ongoing movement and to defeat the
+            Shogun. We will need to deploy funds to develop and grow.{" "}
+            <strong>KIRA's</strong> earn together and work together to raise
+            awareness and build a reserve of currency that goes towards the
+            project and end goal. The Treasury (royalties) will be used
+            primarily to build and deploy incentives and advancements to the
+            community, starting off with weekly deposits to the{" "}
+            <strong>KIRA RESERVE .</strong> <strong>KIRA RESERVE</strong> is
+            built exclusively for holders and will not be used by the team for
+            anything that does not pertain to bettering <strong>KIRA</strong> as
+            a project.
+          </p>
+          <h1>Tokenomics</h1>
+          <img src={ChartBlur} alt="" className="Chartblur" />
+          <img src={TBA} alt="" id="aboveChart" />
         </div>
         <div className="bars">
-        <img src={Bars} alt="" className="BarBlur"/>
-        <img src={TBA} alt="" id="aboveBar"/>
+          <img src={Bars} alt="" className="BarBlur" />
+          <img src={TBA} alt="" id="aboveBar" />
         </div>
       </div>
     </>
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,8 @@
 import Navbar from "../components/Navbar";
-import Search from '../assets/search.png'
-import Add from '../assets/add.png'
-import YourWallet from '../assets/yourwallet.png'
-import Random from '../assets/random.png'
+import Search from "../assets/search.png";
+import Add from "../assets/add.png";
+import YourWallet from "../assets/yourwallet.png";
+import Random from "../assets/random.png";
 // import CharOne from '../assets/CharOne.png'
 // import CharTwo from '../assets/CharTwo.png'
 // import CharThree from '../assets/CharThree.png'
@@ -15,75 +15,100 @@ import Random from '../assets/random.png'
 // import CharTen from '../assets/CharTen.png'
 // import CharEleven from '../assets/CharEleven.png'
 // import CommingSoon from '../assets/Coming soon.png'
-import MarketPlace from '../assets/Marketplace.png'
+import MarketPlace from "../assets/Marketplace-coming soon.png";
 import Staking from "../assets/staking.png";
 const Gallery = () => {
-    return ( 
-     <div className="gal_back">
-     <div className="navBox">
-     <Navbar home = '' market ={Staking} purpose = '' development = '' reserve = '' staking = '' weapon = '' land = ''/>
-     </div>
-     <div className="gal_wrap">
-     <div className="gallery">
-     <div className="filter">
-     <h1>FILTERS</h1>
-     <div className="searchToken">
-     <img src={Search} alt="" />
-     <input type="search" placeholder="Search by token id..." />
-     </div>
-     <div className="Background" id='back'>
-     <div>
-     <p>Background  </p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-
-     </div>
-     <div className="Background">
-     <div>
-     <p>Headwear</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     <div className="Background">
-     <div>
-     <p>Weapon</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     <div className="Background">
-     <div>
-     <p>Eyewear</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     <div className="Background">
-     <div>
-     <p>Mouth</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     <div className="Background">
-     <div>
-     <p>Hair</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     <div className="Background" id="access">
-     <div>
-     <p>Accessories</p>
-     </div>
-     <div className="backImg"><img src={Add} alt="" /></div>
-     </div>
-     </div>
-     </div>
-     <div className="genesis">
-     <div className="genesis_header">
-     <div>
-     <h1>KIRA GENESIS &nbsp; &nbsp; &nbsp; &nbsp; <span>// 10000</span></h1>
-    <button className="randomBtn">
-     <img src={Random} alt="" />
-    Randomize</button>
-     {/* <div className="Characters">
+  return (
+    <div className="gal_back">
+      <div className="navBox">
+        <Navbar
+          home=""
+          market={Staking}
+          purpose=""
+          development=""
+          reserve=""
+          staking=""
+          weapon=""
+          land=""
+        />
+      </div>
+      <div className="gal_wrap">
+        <div className="gallery">
+          <div className="filter">
+            <h1>FILTERS</h1>
+            <div className="searchToken">
+              <img src={Search} alt="" />
+              <input type="search" placeholder="Search by token id..." />
+            </div>
+            <div className="Background" id="back">
+              <div>
+                <p>Background </p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background">
+              <div>
+                <p>Headwear</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background">
+              <div>
+                <p>Weapon</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background">
+              <div>
+                <p>Eyewear</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background">
+              <div>
+                <p>Mouth</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background">
+              <div>
+                <p>Hair</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+            <div className="Background" id="access">
+              <div>
+                <p>Accessories</p>
+              </div>
+              <div className="backImg">
+                <img src={Add} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="genesis">
+          <div className="genesis_header">
+            <div>
+              <h1>
+                KIRA GENESIS &nbsp; &nbsp; &nbsp; &nbsp; <span>// 10000</span>
+              </h1>
+              <button className="randomBtn">
+                <img src={Random} alt="" />
+                Randomize
+              </button>
+              {/* <div className="Characters">
      <div className="CharRowOne">
      <div className="charCont">
      <img src={CharOne} alt="" />
@@ -181,25 +206,28 @@ const Gallery = () => {
      </div>
      </div>
       */}
-     {/* </div> */}
+              {/* </div> */}
 
-<div className="gallery_comming">
-  <img src={MarketPlace} alt="" />
-</div>
+              <div className="gallery_comming">
+                <img
+                  className="marketplace__comingsoon"
+                  src={MarketPlace}
+                  alt=""
+                />
+              </div>
+            </div>
 
-     </div>
+            <div>
+              <button className="walletBtn">
+                <img src={YourWallet} alt="" />
+                Your Wallet
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-     <div>
- <button className="walletBtn">
-     <img src={YourWallet} alt="" />
-     Your Wallet
-    </button>
-     </div>
-     </div>
-     </div>
-     </div>
-     </div>
-     );
-}
- 
 export default Gallery;
